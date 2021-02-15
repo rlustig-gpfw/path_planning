41ca8ed99b0752cf09367ec83828527e40dc8c3c
Ryan Lustig <lustig@braincorporation.com>
2021-02-14 17:47:03 -0800

    Add drawing map drawing capability

--- costmap.py
+++ costmap.py
@@ -1,8 +1,19 @@
 import random
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Sequence, Dict
 
 import numpy as np
+from PIL import Image, ImageDraw
 from attr import attrs
+
+from utils import clamp
+
+
+class Colors:
+    WHITE = (255, 255, 255)
+    GRAY = (150, 150, 150)
+    BLACK = (0, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
 
 
 class Items(object):
@@ -19,8 +30,10 @@ class Costmap(object):
     cols: int
     robot: Tuple[int, int]
     goal: Tuple[int, int]
+
     _data: 'Array[M,N]'
     _cell_size: int = 25
+    _items_to_colors_mapping: Optional[Dict] = None
 
     @classmethod
     def create_map(
@@ -44,17 +57,33 @@ class Costmap(object):
         print(f"Robot: {robot}")
         print(f"Goal: {goal}")
 
-        return Costmap(rows, cols, robot, goal, map, cell_size)
+        items_to_colors_mapping = cls.get_item_to_colors_mapping()
+
+        return Costmap(
+            rows=rows,
+            cols=cols,
+            robot=robot,
+            goal=goal,
+            data=map,
+            cell_size=cell_size,
+            items_to_colors_mapping=items_to_colors_mapping)
 
     def get_data(self):
         return self._data
 
-    def print(self):
-        for r in range(0, self.rows):
-            print()
-            for c in range(0, self.cols):
-                print(self._data[r, c], end=" ")
-        print()
+    def get_neighbors(self, row: int, col: int) -> Optional[Sequence]:
+        neighbors = []
+        min_row = clamp(row - 1, 0, self.rows - 1)  #max(min(row - 1, self.rows + 1), self.rows + 1)  #row - 1 if row > 0 else 0
+        max_row = clamp(row + 1, 0, self.rows - 1)
+        min_col = clamp(col - 1, 0, self.cols - 1)
+        max_col = clamp(col + 1, 0, self.cols - 1)
+        for r in range(min_row, max_row + 1):
+            for c in range(min_col, max_col + 1):
+                if r == row and c == col:
+                    continue
+                if self._data[r, c] == Items.OPEN:
+                    neighbors.append((r, c))
+        return neighbors
 
     def set_robot(self, robot_loc_rowcol: Tuple[int, int]):
         self._data[self.robot[0], self.robot[1]] = Items.OPEN
@@ -64,12 +93,52 @@ class Costmap(object):
         self._data[self.goal[0], self.goal[1]] = Items.OPEN
         self._data[goal_loc_rowcol[0], goal_loc_rowcol[1]] = Items.GOAL
 
+    def set_value(self, rowcol: Tuple[int, int], value: Items):
+        self._data[rowcol[0], rowcol[1]] = value
+
+    @classmethod
+    def get_item_to_colors_mapping(cls):
+        items_to_colors_mapping = {
+            Items.OPEN: Colors.WHITE,
+            Items.OBSTACLE: Colors.BLACK,
+            Items.ROBOT: Colors.BLUE,
+            Items.GOAL: Colors.GREEN
+        }
+        return items_to_colors_mapping
+
+    def print(self):
+        for r in range(0, self.rows):
+            print()
+            for c in range(0, self.cols):
+                print(self._data[r, c], end=" ")
+        print()
+
+    def draw(self):
+        if not self._items_to_colors_mapping:
+            self._items_to_colors_mapping = Costmap.get_item_to_colors_mapping()
+
+        image_size = self.cols * self._cell_size, self.rows * self._cell_size
+        grid = Image.new("RGB", image_size, Colors.WHITE)
+        draw = ImageDraw.Draw(grid)
+
+        for r in range(0, self.rows):
+            for c in range(0, self.cols):
+                # Find top left and bottom right box corners
+                top_left = c * self._cell_size, r * self._cell_size
+                btm_right = (c + 1) * self._cell_size, (r + 1) * self._cell_size
+                box = (top_left, btm_right)
+                # Draw box and fill with desired color
+                fill_color = self._items_to_colors_mapping[self._data[r, c]]
+                draw.rectangle(box, fill=fill_color, outline=Colors.GRAY)
+        del draw
+        grid.show()
+
 
 def generate_random_rowcol(max_row: int, max_col: int) -> Tuple[int, int]:
     return random.randint(0, max_row - 1), random.randint(0, max_col - 1)
 
 
-def generate_random_costmap(rows: int = 5, cols: int = 5, obstacle_percentage: float = 0.25):
+def generate_random_costmap(rows: int = 10, cols: int = 10, obstacle_percentage: float = 0.25):
 
     assert rows > 1 and cols > 1
     obstacles = np.random.rand(rows, cols) <= obstacle_percentage
@@ -93,7 +162,8 @@ def generate_random_costmap(rows: int = 5, cols: int = 5, obstacle_percentage: f
 
 
 if __name__ == "__main__":
-    # m = Costmap.create_map(7, 7, robot=(2, 2), goal=(5, 6))
-    # m.print()
-    costmap = generate_random_costmap()
+    costmap = generate_random_costmap(20, 30)
     costmap.print()
+
+    print(costmap.get_neighbors(0, 0))
+    costmap.draw()
---
+++ utils.py
@@ -0,0 +1,4 @@
+
+
+def clamp(num, smallest, largest):
+    return max(smallest, min(num, largest))
